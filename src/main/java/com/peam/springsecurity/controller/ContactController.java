78bc92eb2c3aade5804ca8f2014fe67c6c46acd0
yourisev <sevgama44@gmail.com>
2024-02-18 12:43:19 -0500

    Added publicly available endpoints.

---
+++ src/main/java/com/peam/springsecurity/controller/ContactController.java
@@ -0,0 +1,13 @@
+package com.peam.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ContactController {
+
+    @GetMapping("/contact")
+    public String saveContactInquiryDetails(){
+        return "Requested Info sent, you'll be contacted";
+    }
+}
