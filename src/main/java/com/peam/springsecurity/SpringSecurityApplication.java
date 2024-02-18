11c4fb0379120778573e968a250bc5e8520db4e9
yourisev <sevgama44@gmail.com>
2024-02-17 20:11:20 -0500

    Initial commit

---
+++ src/main/java/com/peam/springsecurity/SpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.peam.springsecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/peam/springsecurity/controller/WelcomeController.java
@@ -0,0 +1,13 @@
+package com.peam.springsecurity.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome")
+    public String sayWelcome(){
+        return "Welcome to this Spring Application without Spring Security";
+    }
+}
