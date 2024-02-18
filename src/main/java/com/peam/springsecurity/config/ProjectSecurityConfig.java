6c1e35a0ce0b36a92e1235f88d8a63b77834b497
yourisev <sevgama44@gmail.com>
2024-02-18 14:37:04 -0500

    Using the default password encoder for creating user details meaning the user password is not encoded and kept in plane text.

--- src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
@@ -5,6 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -20,5 +23,27 @@ public class ProjectSecurityConfig {
         http.formLogin(Customizer.withDefaults());
         http.httpBasic(Customizer.withDefaults());
         return (SecurityFilterChain)http.build();
+    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService(){
+        /*
+        Approach using the DefaultPasswordEncoder() to create the in memory user details
+         */
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("dev1")
+                .password("126974")
+                .authorities("admin")
+                .build();
+
+        UserDetails user2 = User.withDefaultPasswordEncoder()
+                .username("user1")
+                .password("004653")
+                .authorities("read")
+                .build();
+
+        return new InMemoryUserDetailsManager(user1, user2);
+
     }
 }
