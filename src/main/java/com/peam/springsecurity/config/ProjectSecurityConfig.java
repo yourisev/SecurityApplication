4dd57b06e424e6117cc0a365141a3afbe3bfbb0b
yourisev <sevgama44@gmail.com>
2024-02-18 18:05:34 -0500

    We defined a custom UserDetails service implementation that fetches a user from the customer table in the database using the email and builds the UserDetails for this user and sends to the AuthenticationProvider

--- src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
@@ -31,11 +31,6 @@ public class ProjectSecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService(DataSource dataSource){
-        return new JdbcUserDetailsManager(dataSource);
-    }
-
-    @Bean
     public PasswordEncoder passwordEncoder(){
         return NoOpPasswordEncoder.getInstance();
     }
