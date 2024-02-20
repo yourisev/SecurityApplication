5c4cc378b420342ed5fcdc58f0d8da736dd0f0b1
yourisev <sevgama44@gmail.com>
2024-02-19 22:20:12 -0500

    moved to repository package and changed its referenced Id field.

--- src/main/java/com/peam/springsecurity/repository/CustomerRepository.java
+++ src/main/java/com/peam/springsecurity/repository/CustomerRepository.java
@@ -1,4 +1,4 @@
-package com.peam.springsecurity;
+package com.peam.springsecurity.repository;
 
 import com.peam.springsecurity.model.Customer;
 import org.springframework.data.repository.CrudRepository;
@@ -7,7 +7,8 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface CustomerRepository extends CrudRepository<Customer,Integer> {
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
 
     List<Customer> findByEmail(String email);
+
 }
