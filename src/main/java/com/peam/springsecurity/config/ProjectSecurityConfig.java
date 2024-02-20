580f0ec45e7f5c1e13469c113ce202a816d72840
yourisev <sevgama44@gmail.com>
2024-02-19 22:23:53 -0500

    required the /user endpoint should request authentication before accessing and fixed CORS to give access from http://localhost:4200 to all methods, allowing them to share credentials, allowing all headers and keeping this permission for an hour on every request.

--- src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.peam.springsecurity.config;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -13,16 +14,31 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 
 import javax.sql.DataSource;
+import java.util.Collections;
 
 @Configuration
 public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.csrf((csrf) -> csrf.disable()).authorizeHttpRequests((requests) -> {
-            requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards").authenticated()
+        http.cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration configuration = new CorsConfiguration();
+                        configuration.setAllowedOrigins(Collections.singletonList("http://localhost:4200"));
+                        configuration.setAllowedMethods(Collections.singletonList("*"));
+                        configuration.setAllowCredentials(true);
+                        configuration.setAllowedHeaders(Collections.singletonList("*"));
+                        configuration.setMaxAge(3600L);
+                        return configuration;
+                    }
+                }) ).
+                csrf((csrf) -> csrf.disable()).authorizeHttpRequests((requests) -> {
+            requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards","/user").authenticated()
                     .requestMatchers("/contact","/notices","/register").permitAll()
                     .anyRequest().permitAll();
         });
