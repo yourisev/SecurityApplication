97fe4991483226477345a7d2265ced84ed432506
yourisev <sevgama44@gmail.com>
2024-02-20 22:12:03 -0500

    updated the line for ignoring CSRF tokens on contact and register endpoints to have a csrf token request handler. We added our custom filter after the BasicAuthentication Filter to handle our CSRF token added code to give control to Spring Security to save the security context automatically. We equally made our application to always create a new session per request if one does not exist

--- src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
+++ src/main/java/com/peam/springsecurity/config/ProjectSecurityConfig.java
@@ -1,10 +1,12 @@
 package com.peam.springsecurity.config;
 
+import com.peam.springsecurity.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -14,6 +16,9 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -25,7 +30,12 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        http.cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
+
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
+        http.securityContext((context) -> {context.requireExplicitSave(false);}).
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors((cors) -> cors.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration configuration = new CorsConfiguration();
@@ -37,8 +47,10 @@ public class ProjectSecurityConfig {
                         return configuration;
                     }
                 }) ).csrf((csrf) -> {
-                    csrf.ignoringRequestMatchers("/contact","/register");
-                }).
+                    csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers("/contact","/register").
+                            csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse());
+
+                }).addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class).
                 authorizeHttpRequests((requests) -> {
             requests.requestMatchers("/myAccount","/myBalance","/myLoans","/myCards","/user").authenticated()
                     .requestMatchers("/contact","/notices","/register").permitAll()
