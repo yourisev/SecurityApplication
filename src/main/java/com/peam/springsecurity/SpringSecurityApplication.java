5341747589ab4ae26c8353f41b7d8c2c964adfd1
yourisev <sevgama44@gmail.com>
2024-02-22 07:22:28 -0500

    Some cleaning.

--- src/main/java/com/peam/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/peam/springsecurity/SpringSecurityApplication.java
@@ -2,6 +2,7 @@ package com.peam.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
