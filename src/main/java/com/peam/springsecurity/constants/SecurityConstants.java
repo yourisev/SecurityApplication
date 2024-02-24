641790317ecb7a1d88b3cba93c4257671a67f12a
yourisev <sevgama44@gmail.com>
2024-02-24 10:10:55 -0500

    Added JWT dependencies and defined some security constants (JWT key and header name).

---
+++ src/main/java/com/peam/springsecurity/constants/SecurityConstants.java
@@ -0,0 +1,7 @@
+package com.peam.springsecurity.constants;
+
+public interface SecurityConstants {
+
+    public static final String JWT_KEY ="xxeRj6N3VoKdYPtXBINlFdwnb6gUXPRuXP_eWETb65s";
+    public static final String JWT_HEADER ="Authorization";
+}
