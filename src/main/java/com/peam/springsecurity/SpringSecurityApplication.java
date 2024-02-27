8ae6d55f3cfc831e6abc193ea8788044a5c519f8
yourisev <69630866+yourisev@users.noreply.github.com>
2024-02-26 19:20:41 -0500

    Revert "O auth2"

--- src/main/java/com/peam/springsecurity/SpringSecurityApplication.java
+++ src/main/java/com/peam/springsecurity/SpringSecurityApplication.java
@@ -2,8 +2,6 @@ package com.peam.springsecurity;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @SpringBootApplication
 public class SpringSecurityApplication {
